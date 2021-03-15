b5da66646bb0e7422c2cbf744ce23449a134f656
Georgi Kolev <georgi.kolev@gmail.com>
2021-03-15 18:34:42 +0200

    preparing for eks

---
+++ deploy/kafka-connect-splunk/templates/_helpers.tpl
@@ -0,0 +1,45 @@
+{{/* vim: set filetype=mustache: */}}
+{{/*
+Expand the name of the chart.
+*/}}
+{{- define "kafka-connect-splunk.name" -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{/*
+Create a default fully qualified app name.
+We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
+If release name contains chart name it will be used as a full name.
+*/}}
+{{- define "kafka-connect-splunk.fullname" -}}
+{{- if .Values.fullnameOverride -}}
+{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- $name := default .Chart.Name .Values.nameOverride -}}
+{{- if contains $name .Release.Name -}}
+{{- .Release.Name | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+{{- end -}}
+{{- end -}}
+
+{{/*
+Create chart name and version as used by the chart label.
+*/}}
+{{- define "kafka-connect-splunk.chart" -}}
+{{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{/*
+Common labels
+*/}}
+{{- define "kafka-connect-splunk.labels" -}}
+app.kubernetes.io/name: {{ include "kafka-connect-splunk.name" . }}
+helm.sh/chart: {{ include "kafka-connect-splunk.chart" . }}
+app.kubernetes.io/instance: {{ .Release.Name }}
+{{- if .Chart.AppVersion }}
+app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
+{{- end }}
+app.kubernetes.io/managed-by: {{ .Release.Service }}
+{{- end -}}
---
+++ docker/entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+APP_NAME="kafka-connect-splunk"
+TMPFL="/tmp/.tmpfl"
+
+if [ -z ${ENVIRONMENT} ]; then
+  ENVIRONMENT=$1
+fi
+
+case $ENVIRONMENT in
+    test|tests) ENV=tests;;
+	localdev|dev|development) ENV=dev;;
+	master|production|prod|parent) ENV=production;;
+	*) ENV=staging;;
+esac
+
+echo -e "Env: $ENV"
+
+# Production role change
+if [ "$ENV" == "production" ]; then
+    echo "Replace AWS Credentials role with production one..."
+    sed -i 's#arn:aws:iam::593202966396:role/mmlf-staging-eks-eks-node-role#arn:aws:iam::593202966396:role/production-eks-NodeInstanceRole-PF0E41JLO9OV#' /srv/.aws/credentials
+fi
+
+echo -e "Fetching parameters from AWS SSM Parameter Store for Environment: [$ENV]..."
+aws ssm get-parameters-by-path --output text --with-decryption --recursive --path /${ENV}/app/global | sed "s#/app/global##g" | awk '{print $4,$6}' > $TMPFL
+aws ssm get-parameters-by-path --output text --with-decryption --recursive --path /${ENV}/app/${APP_NAME} | sed "s#/app/${APP_NAME}##g" | awk '{print $4,$6}' >> $TMPFL
+
+for TEMPLATE in $(ls docker/*.template); do
+    FILE="$(echo $TEMPLATE | sed "s#.template##g")"
+    cp -f $TEMPLATE $FILE
+    echo "Generating $FILE"
+    while read line; do
+        larray=( $line )
+        PARAM="$(echo ${larray[0]^^} | tr "/" "_" | sed "s#_${ENV^^}_##g" )"
+        sed -i "s#{{${PARAM}}}#${larray[1]}#g" $FILE 2>/dev/null || sed -i "s|{{${PARAM}}}|${larray[1]}|g" $FILE
+    done < $TMPFL
+done
+
+rm $TMPFL
+
+# Replace configs with the generated files
+mv -f /docker/env.template /docker/env
+#export $(xargs <file)
+
+# Start the app
+/docker/init.sh
